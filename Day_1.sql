daeff0fd84975c5943ff913d7abc36aa7952646c
Abdullah Akintobi <akintobiabdullah@gmail.com>
2024-10-31 15:44:43 +0100

    Update Day_1.sql

--- Day_1.sql
+++ Day_1.sql
@@ -5,3 +5,37 @@ Your result cannot contain duplicates.
 SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY REGEXP '^[aeiouAEIOU]';
+--
+/*
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. 
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '[aeiouAEIOU]$';
+--
+/*
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) 
+as both their first and last characters. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION 
+WHERE CITY REGEXP '^[AEIOUaeiou]'
+        AND CITY REGEXP '[AEIOUaeiou]$';
+--
+/*
+Query the list of CITY names from STATION that do not start with vowels. 
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT REGEXP '^[AEIOUaeiou]';
+--
+/*
+Query the list of CITY names from STATION that do not end with vowels. 
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT REGEXP '[AEIOUaeiou]$';
+
