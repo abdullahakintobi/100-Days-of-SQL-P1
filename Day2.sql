ea24083e8310574b31a9e53b8c19ff1d20677c29
Abdullah Akintobi <akintobiabdullah@gmail.com>
2024-11-01 12:53:44 +0100

    Update Day2.sql

--- Day2.sql
+++ Day2.sql
@@ -78,3 +78,15 @@ SELECT
     ROUND(SUM(LONG_W), 2) AS lon
 FROM
     STATION;
+--
+/*
+Query the list of CITY names from STATION that either do not start with vowels 
+or do not end with vowels. Your result cannot contain duplicates.
+*/
+SELECT
+    DISTINCT(CITY)
+FROM
+    STATION
+WHERE
+    CITY NOT REGEXP '^[AEIOUaeiou]'
+     OR CITY NOT REGEXP '[AEIOUaeiou]$';
