fd415eced2f153e53b9935f39ceee0f4fd02e994
Abdullah Akintobi <akintobiabdullah@gmail.com>
2024-11-01 12:46:41 +0100

    Update Day2.sql

--- Day2.sql
+++ Day2.sql
@@ -67,3 +67,14 @@ FROM
 WHERE
     COUNTRYCODE = 'USA'
         AND POPULATION > 100000;
+--
+/*
+Query the following two values from the STATION table:
+The sum of all values in LAT_N rounded to a scale of  decimal places.
+The sum of all values in LONG_W rounded to a scale of  decimal places.
+*/
+SELECT
+    ROUND(SUM(LAT_N), 2) AS lat,
+    ROUND(SUM(LONG_W), 2) AS lon
+FROM
+    STATION;
