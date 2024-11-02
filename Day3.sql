20712bc3b8ab47cfa605c39d95240bf3fb80f268
Abdullah Akintobi <akintobiabdullah@gmail.com>
2024-11-02 23:45:54 +0100

    Update Day3.sql

--- Day3.sql
+++ Day3.sql
@@ -31,3 +31,11 @@ FROM
     CITY
 WHERE
     DISTRICT = 'California';
+--
+/*
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+*/
+SELECT
+    ROUND(AVG(POPULATION), 0) AS AVG_POPULATION
+FROM
+    CITY;
