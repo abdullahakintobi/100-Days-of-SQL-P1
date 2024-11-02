7122ac72c216585029eb9c64e0d82b79f7107dc8
Abdullah Akintobi <akintobiabdullah@gmail.com>
2024-11-03 00:18:25 +0100

    Update Day3.sql

--- Day3.sql
+++ Day3.sql
@@ -79,3 +79,16 @@ SELECT
         AS POPULATION_DIFF
 FROM
     CITY;
+/*
+Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES
+table, but did not realize her keyboard's 0 key was broken until after completing the calculation. She
+wants your help finding the difference between her miscalculation (using salaries with any zeros removed),
+and the actual average salary. 
+
+Write a query calculating the amount of error (i.e.: actual - miscalculated average monthly salaries), 
+and round it up to the next integer. 
+*/
+SELECT 
+    CEIL(AVG(Salary) - AVG(CAST(REPLACE(CAST(Salary AS CHAR), '0', '') AS UNSIGNED))) AS AVG_ERROR
+FROM 
+    EMPLOYEES;
