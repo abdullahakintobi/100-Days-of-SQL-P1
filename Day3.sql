4fb0ff1c4b8abdf4dba25c8cfa5e9c24555749e8
Abdullah Akintobi <akintobiabdullah@gmail.com>
2024-11-02 23:53:38 +0100

    Update Day3.sql

--- Day3.sql
+++ Day3.sql
@@ -59,3 +59,14 @@ FROM
     CITY
 WHERE
     DISTRICT = 'California';
+--
+/*
+Query the sum of the populations for all Japanese cities in CITY. 
+The COUNTRYCODE for Japan is JPN..
+*/
+SELECT
+    SUM(POPULATION) AS SUM_POPULATION
+FROM
+    CITY
+WHERE
+    COUNTRYCODE = 'JPN';
