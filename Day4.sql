319f53d33eafcfd5676fc7edf1364e54e7745313
Abdullah Akintobi <akintobiabdullah@gmail.com>
2024-11-03 23:02:18 +0100

    Update Day4.sql

--- Day4.sql
+++ Day4.sql
@@ -12,4 +12,18 @@ JOIN
     COUNTRY ON CITY.CountryCode = COUNTRY.Code
 WHERE 
     COUNTRY.CONTINENT = 'Africa';
+--
+/*
+Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the
+CONTINENT is 'Asia'.
 
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+SELECT 
+    SUM(CITY.Population) AS Total_Population
+FROM 
+    CITY
+JOIN 
+    COUNTRY ON CITY.CountryCode = COUNTRY.Code
+WHERE 
+    COUNTRY.CONTINENT = 'Asia';
