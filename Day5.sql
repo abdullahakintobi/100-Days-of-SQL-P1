9ece6a91aa5b6692f7b2ee5ba5f483393837fd5f
Abdullah Akintobi <akintobiabdullah@gmail.com>
2024-11-08 22:50:44 +0100

    Update Day5.sql

--- Day5.sql
+++ Day5.sql
@@ -15,6 +15,7 @@ LIMIT 1;
 /*
 Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. 
 Output one of the following statements for each record in the table:
+
 - Equilateral: It's a triangle with 3 sides of equal length. 
 - Isosceles: It's a triangle with 2 sides of equal length. 
 - Scalene: It's a triangle with 3 sides of differing lengths. 
