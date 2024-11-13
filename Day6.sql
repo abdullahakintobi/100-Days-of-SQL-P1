9e72e5da0c80b992b1b891a59a47f1ff699bfd18
Abdullah Akintobi <akintobiabdullah@gmail.com>
2024-11-13 23:57:48 +0100

    Update Day6.sql

--- Day6.sql
+++ Day6.sql
@@ -2,12 +2,11 @@
 -- Lab 3 - Composing and Running Basic SQL Queries
 -- Hands-on Lab: Composing and Running Basic SQL Queries
 -- RDBMS - IBM DB2
---
---
+
 -- Drop table if it exists
 DROP TABLE
   IF EXISTS INSTRUCTOR;
---
+
 -- Create table
 CREATE TABLE
   INSTRUCTOR (
@@ -17,23 +16,22 @@ CREATE TABLE
     city VARCHAR(15),
     country CHAR(2)
   );
---
+
 -- Insert a record into the INSTRUCTOR table
 INSERT INTO
   INSTRUCTOR (ins_id, lastname, firstname, city, country)
 VALUES
   ('1', 'Ahuja', 'Rav', 'Toronto', 'CA');
---
+
 -- Insert two records into the INSTRUCTOR table
 INSERT INTO
   INSTRUCTOR (ins_id, lastname, firstname, city, country)
 VALUES
   ('2', 'Chong', 'Raul', 'Toronto', 'CA'),
   ('3', 'Vasudevan', 'Hima', 'Chicago', 'US');
---
+
 -- Select all rows from INSTRUCTOR table
 SELECT
   *
 FROM
   INSTRUCTOR;
---
